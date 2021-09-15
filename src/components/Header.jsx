386cc0da0c5c00bc0188042847563424a99bff63
Roman Humennyi <duklagos@gmail.com>
2021-09-15 22:38:07 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import DoneOutlineIcon from "@material-ui/icons/DoneOutline";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <DoneOutlineIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
